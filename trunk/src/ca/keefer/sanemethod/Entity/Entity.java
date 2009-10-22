53148145e65b590ef6be3dffc9d96eb8b04d8b4d
SaneMethodology <SaneMethodology@7904a09a-addc-11de-8539-5f84f9253713>
2009-10-22 17:24:20 +0000

    Finished most of Player implementation. Physics working, for the most part. Will now turn attention to the sub-project of building a mapping utility with which I can visually draw the various shapes, which will be appropriately textured, and output these drawings into Slick code. Wish me luck.

--- trunk/src/ca/keefer/sanemethod/Entity/Entity.java
+++ trunk/src/ca/keefer/sanemethod/Entity/Entity.java
@@ -1,59 +1,38 @@
 package ca.keefer.sanemethod.Entity;
 
+
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.geom.Rectangle;
 
 /**
- * This interface defines some of the attributes and methods all classes implementing the Entity
- * interface must possess.
+ * This interface defines the methods all classes implementing the Entity interface must possess.
  * @author Christopher Keefer
  * @version 1.1
- * @see ca.keefer.sanemethod.SomeObject
+ * @see ca.keefer.sanemethod.PhysObject
  * @see ca.keefer.sanemethod.Sprite
  * @see ca.keefer.sanemethod.Player
  *
  */
-public interface Entity {
 
-	/**
-	 * Get the x position of this Entity 
-	 * @return float x position
-	 */
-	public float getXPosition();
+public interface Entity {
 	
 	/**
-	 * Get the y position of this Entity
-	 * @return float y position
+	 * Get the bounding box for this Entity based on its current position
+	 * and the currently displaying animation image 
+	 * @return
 	 */
-	public float getYPosition();
+	public Rectangle getBoundingBox();
 	
 	/**
-	 * Set the x position of this Entity
-	 * @param x float x position to set
-	 */
-	public void setXPosition(float x);
-	
-	/**
-	 * Set the y position of this Entity
-	 * @param y float y position to set
-	 */
-	public void setYPosition(float y);
-	
-	/**
-	 * Draw this entity at its given x,y position
-	 * @param g Graphics context to draw to
-	 */
-	public void render(Graphics g);
-	
-	/**
-	 * Update this entity's position, state, etc.
+	 * Update this entity's position, state, etc. during the physical loop.
+	 * Called each time the World is updated.
 	 * @param delta int Time elapsed since last update
 	 */
 	public void update(int delta);
 	
-	public Rectangle getBoundingRectangle();
-	
-	public boolean collides(Entity e);
-	
-	public void impulse(Entity e);
+	/**
+	 * Draw this entity at its given Body position
+	 * @param g Graphics context to draw to
+	 */
+	public void render(Graphics g);
 }
---
+++ trunk/src/ca/keefer/sanemethod/Entity/Player.java
@@ -0,0 +1,185 @@
+package ca.keefer.sanemethod.Entity;
+
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.util.Log;
+
+import ca.keefer.sanemethod.Constants;
+import ca.keefer.sanemethod.Environment.TiledHandler;
+
+public class Player implements Entity{
+
+	float xPos; // x position of sprite
+	float yPos; // y position of sprite
+	int height; // height of the sprite
+	int width; // width of the sprite
+	TiledHandler tHandler;
+	Vector2f velocity;
+	float mass; // mass of the sprite
+	Image thisImage; // temporary image for testing movement
+	double velocityX;
+	double velocityY;
+	boolean direction;
+	boolean moving;
+	boolean jumping;
+	float accel = Constants.ACCELERATION;
+	
+	// Constants for Direction
+	public static final boolean DIR_LEFT = true;
+	public static final boolean DIR_RIGHT = false;
+	
+	public Player (float x, float y, Image image, TiledHandler tHandler){
+		this.xPos=x;
+		this.yPos=y;
+		this.mass = 1;
+		this.tHandler = tHandler;
+		this.velocity = new Vector2f(0,0);
+		this.thisImage=image;
+		this.velocityX=0;
+		this.velocityY=0;
+		this.direction = DIR_LEFT;
+		this.moving=false;
+		this.jumping=false;
+	}
+	
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		g.drawImage(thisImage, xPos, yPos);
+	}
+	
+	public void setDirection(boolean t){
+		direction = t;
+	}
+	public void setMoving(boolean t){
+		moving =t;
+	}
+	
+	public void setJumping (boolean t){
+		jumping=t;
+	}
+	public void setX(float x){
+		this.xPos = x;
+	}
+	public void setY(float y){
+		this.yPos = y;
+	}
+	
+	public void setVelocityY(float velY){
+		velocity.y = velY;
+	}
+	public void setVelocityX(float velX){
+		velocity.x = velX;
+	}
+	public void setVelocity(float x, float y){
+		velocity = new Vector2f(x,y);
+	}
+	
+	public float getVelocityX(){
+		return velocity.getX();
+	}
+	
+	public float getVelocityY(){
+		return velocity.getY();
+	}
+	
+	public Vector2f getVelocity(){
+		return velocity;
+	}
+	
+	public boolean getDirection(){
+		return direction;
+	}
+
+	
+	@Override
+	public void update(int delta) {
+		// Call movement calculation
+		move(delta);
+	}
+	
+	public void move(int delta){
+		if (jumping){
+			if (this.yPos <= 40){
+			//this.yPos -= velocityY;
+				this.yPos -= velocity.getY();
+			}else {
+				this.yPos = 40;
+				jumping =false;
+			}
+		
+		}
+		
+		
+		if (velocity.getY() > -500){
+			velocity.y -= Constants.GRAVITY.y + Constants.FRICTION * velocity.getY();
+		}else{
+			velocity.y = 0;
+		}
+		
+		if (moving){
+			//Modeling velocity as an implementation of Stokes' Drag (v = (a/f) - (a/f)*exp(-f*t))
+			velocity.x = (float) ((accel/Constants.FRICTION) - (accel/Constants.FRICTION)*Math.exp(-Constants.FRICTION*delta));
+
+			// scale velocity
+			if (accel < Constants.MAX_ACCELERATION){
+				accel += 0.01;
+			}
+		}else{
+			accel = Constants.ACCELERATION;
+			if (velocity.getX() > 0.0001){
+				velocity.x -= Constants.FRICTION * velocity.getX();
+			}else{
+				velocity.x=0;
+			}
+		}
+		if (direction == DIR_LEFT){
+			this.xPos -= velocity.getX();
+		}else{
+			this.xPos += velocity.getX();
+		}
+	}
+
+	/**
+	 * This method takes a keypress event from the state and interprets it into action
+	 * for this Entity.
+	 * @param keyPressed
+	 */
+	public void receiveKeyPress(int keyPressed){
+		if (keyPressed == Constants.KEY_LEFT){
+			this.setDirection(DIR_LEFT);
+			this.setMoving(true);
+		}else if (keyPressed == Constants.KEY_RIGHT){
+			this.setDirection(DIR_RIGHT);
+			this.setMoving(true);
+		}else if (keyPressed == Constants.KEY_CANCEL){
+			this.setJumping(true);
+			this.setVelocityY(45);
+		}
+	}
+	
+	/**
+	 * This method takes a keyRelease event from the state and interprets it into action
+	 * or cessation thereof for this Entity.
+	 * @param keyReleased
+	 */
+	public void receiveKeyRelease(int keyReleased){
+		if (keyReleased == Constants.KEY_LEFT){
+			this.setMoving(false);
+		}else if (keyReleased == Constants.KEY_RIGHT){
+			this.setMoving(false);
+		}
+	}
+
+	@Override
+	public Rectangle getBoundingBox() {
+		return new Rectangle(this.xPos,this.yPos,this.thisImage.getWidth(),this.thisImage.getHeight());
+	}
+	
+
+
+}
